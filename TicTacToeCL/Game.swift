dbfc105ac7e8f1e2100b3300d197b25f8b44d6ec
Ali•Gorithm <alisher.coder@gmail.com>
2017-05-21 15:23:35 -0400

    first commit

--- TicTacToeCL/Game.swift
+++ TicTacToeCL/Game.swift
@@ -10,33 +10,32 @@ import Foundation
 
 struct Game {
     
-    let playerXname = getStringFromUser(prompt: "What is player X's name?", failureMessage: "Please Enter you name", predicate: { $0.isEmpty == false } )
-    let playerOname = getStringFromUser(prompt: "What is player O's name?", failureMessage: "Please Enter you name", predicate: { $0.isEmpty == false } )
+    var playerXname = getStringFromUser(prompt: "What is player X's name?", failureMessage: "Please Enter you name", predicate: { $0.isEmpty == false } )
+    var playerOname = getStringFromUser(prompt: "What is player O's name?", failureMessage: "Please Enter you name", predicate: { $0.isEmpty == false } )
     
     var board = BoardState([:])
-    var lastMove: Symbol?
     var turn: Bool = true
-    
+    var spot: Bool = true
     
     mutating func play() {
         
         if winner(moves: board.storage) == .x {
-            print("\(playerXname):\(Symbol.x.description) wins")
+            print("\(playerXname): \(Symbol.x.description.capitalized) wins. \n\(board.description)")
             restart()
         }else if winner(moves: board.storage) == .o {
-            print("\(playerOname):\(Symbol.o.description) wins")
+            print("\(playerOname): \(Symbol.o.description.capitalized) wins. \n\(board.description)")
             restart()
         }else{
             if turn == true {print("\(playerXname)'s turn please choose a move")
-            }else{
-            print("\(playerOname)'s turn please choose a move")
+            }else if turn == false && checkDraw() != (true, "Game tied"){
+                print("\(playerOname)'s turn please choose a move")
             }
         }
         
         guard checkDraw() != (true, "Game tied") else {
-            print(board.description)
+            print("Game between \(playerXname) & \(playerOname) tied.\n\(board.description)")
             restart()
-            return print("Game Tied want to play again")
+            return
         }
         
         
@@ -54,54 +53,68 @@ struct Game {
                 "for the lower right"
             ])
         
+        
         switch choice {
         case "for the left top corner":
-            if turn == true{
-                self.board.storage[0] = (.x); self.turn = false }
-            else{ self.board.storage[0] = (.o); self.turn = true}
+            putInputOnABoard("for the left top corner")
             return play()
         case "for upper middle":
-            if turn == true{
-                self.board.storage[1] = (.x); self.turn = false }
-            else{ self.board.storage[1] = (.o); self.turn = true}
+            putInputOnABoard("for upper middle")
             return play()
         case "for the right top corner":
-            if turn == true{
-                self.board.storage[2] = (.x); self.turn = false }
-            else{ self.board.storage[2] = (.o); self.turn = true}
+            putInputOnABoard("for the right top corner")
             return play()
         case "for the middle left corner":
-            if turn == true{
-                self.board.storage[3] = (.x); self.turn = false }
-            else{ self.board.storage[3] = (.o); self.turn = true}
+            putInputOnABoard("for the middle left corner")
             return play()
         case "for the center":
-            if turn == true{
-                self.board.storage[4] = (.x); self.turn = false }
-            else{ self.board.storage[4] = (.o); self.turn = true}
+            putInputOnABoard("for the center")
             return play()
         case "for the middle right corner":
-            if turn == true{
-                self.board.storage[5] = (.x); self.turn = false }
-            else{ self.board.storage[5] = (.o); self.turn = true}
+            putInputOnABoard("for the middle right corner")
             return play()
         case "for the left lower corner":
-            if turn == true{
-                self.board.storage[6] = (.x); self.turn = false }
-            else{ self.board.storage[6] = (.o); self.turn = true}
+            putInputOnABoard("for the left lower corner")
             return play()
         case "for the lower middle":
-            if turn == true{
-                self.board.storage[7] = (.x); self.turn = false }
-            else{ self.board.storage[7] = (.o); self.turn = true}
+            putInputOnABoard("for the lower middle")
             return play()
         case "for the lower right":
-            if turn == true{
-                self.board.storage[8] = (.x); self.turn = false }
-            else{ self.board.storage[8] = (.o); self.turn = true}
+            putInputOnABoard("for the lower right")
             return play()
         default:
             return print("This should not happen")
+        }
+        
+    }
+    
+    mutating func putInputOnABoard(_ choice: String) {
+        
+        let selectedChoice = getTheChoosenNumber(choice)
+        checkTheGridSpot(selectedChoice - 1)
+        var symbol: Symbol
+        
+        switch turn {
+        case true:
+            symbol = .x
+        case false:
+            symbol = .o
+        }
+        if spot == true {
+            self.board.storage[selectedChoice - 1] = symbol
+            if turn == true {turn = false}else{turn = true}
+        }
+        
+    }
+    
+    mutating func checkTheGridSpot(_ spot: Int) {
+        
+        if self.board.storage[spot] == .x || self.board.storage[spot] == .o  {
+            print("please enter another spot this one is taken")
+            if turn == true {turn = true}else{turn = false}
+            self.spot = false
+        }else{
+            self.spot = true
         }
         
     }
@@ -161,27 +174,53 @@ struct Game {
         }
     }
     
-    mutating func restart() -> Bool {
-        
-        let choice = getChoiceFromUser(prompt: "Would you like to play again?\n",
-                                       choices: [
-                                        "yes",
-                                        "no"
-            ])
-        
+    mutating func restart() {
+        let choice = getChoiceFromUser(prompt: "Would you like to play again? \nChoose no if you want to change names.\n", choices: [ "yes", "no"])
         switch choice {
-        case "Yes":
-            self.board.storage.removeAll()
-            return true
-        case "No":
-            self.board.storage.removeAll()
-            print("Thank you for playing TicTacToe")
-            return true
+        case "yes":
+            self.board.storage = [:]
+            turn = true
+            play()
+        case "no":
+            self.board.storage = [:]
+            turn = true
+            playerXname = getStringFromUser(prompt: "What is player X's name?", failureMessage: "Please Enter you name", predicate: { $0.isEmpty == false } )
+            playerOname = getStringFromUser(prompt: "What is player O's name?", failureMessage: "Please Enter you name", predicate: { $0.isEmpty == false } )
+            play()
         default:
             print("Should not happen")
         }
-        //this could go wrong
-        return false
+    }
+    
+    
+    
+    func getTheChoosenNumber(_ input: String) -> Int {
+        
+        var num = 0
+        
+        switch input {
+        case "for the left top corner":
+            num += 1
+        case "for upper middle":
+            num += 2
+        case "for the right top corner":
+            num += 3
+        case "for the middle left corner":
+            num += 4
+        case "for the center":
+            num += 5
+        case "for the middle right corner":
+            num += 6
+        case "for the left lower corner":
+            num += 7
+        case "for the lower middle":
+            num += 8
+        case "for the lower right":
+            num += 9
+        default:
+            break
+        }
+        return num
     }
     
 }
